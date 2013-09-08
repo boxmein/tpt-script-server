a4e7b98153e2d9ed14b3c415a0e6f316461db374
boxmein <johannes12321@windowslive.com>
2013-09-09 00:30:16 +0300

    Added base code to begin expanding upon

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,35 @@
+var db = require('nedb'); 
+var fs = require('fs');
+var scripts = new db({filename: "databases/scripts.nedb", autoload: true});
 
-/*
- * GET home page.
- */
+var links = [
+  {'href': '/', 'title': 'Home'},
+  {'href': '#about', 'title': 'About'},
+  {'href': '#usage', 'title': 'Usage'},
+  {'href': 'https://github.com/boxmein/tpt-script-server', 'title': 'On GitHub'}
+];
+links.active = '/';
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'TPT Script Server', links: links});
+};
+
+exports.view = function (req, res) {
+  var saveID = req.params.saveID; 
+  scripts.find({id: saveID}, function (err, docs) {
+    if (err) 
+      return res.send("ERR:Error finding script ("+saveID+"): "+err);
+    else if (docs.length == 0)
+      return res.send("ERR:No scripts found");
+  });
+};
+
+exports.raw = function (req, res) {
+  var saveID = req.params.saveID; 
+  scripts.find({id: saveID}, function (err, docs) {
+    if (err) 
+      return res.send("ERR:Error finding script ("+saveID+"): "+err);
+    else if (docs.length == 0)
+      return res.send("ERR:No scripts found");
+  });
 };
