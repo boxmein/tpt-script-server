f8322d30f51b3fe269ac90c367966da75e5fca52
boxmein <johannes12321@windowslive.com>
2013-09-09 00:29:59 +0300

    Added more dummy routes for tomorrow

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 
-/**
- * Module dependencies.
- */
-
 var express = require('express')
   , routes = require('./routes')
-  , user = require('./routes/user')
   , http = require('http')
   , path = require('path');
 
@@ -27,8 +22,14 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/', routes.index);              // View the project index
+
+app.get('/view/:scriptID', routes.view); // View a script with a cute page
+app.get('/raw/:scriptID', routes.raw);   // Request the raw data
+app.get('/meta/:scriptID', routes.meta); // Request metadata only
+
+app.get('/list/', routes.list);          // View a script list prettily
+app.get('/list/raw', routes.rawlist);    // Raw format of the list
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
