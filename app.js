b3440afb8c3d245231a453e6ca863820204f2289
boxmein <johannes12321@windowslive.com>
2013-09-22 01:40:19 +0300

    Added login/assoc related routes

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 
 var express = require('express')
-  , routes = require('./routes')
+  , routes = require('./routes/index.js')
+  , manage = require('./routes/manage.js') 
+  , tptapi = require('./routes/tptapi.js')
+  , G = require('./routes/globals.js')
   , http = require('http')
   , path = require('path');
 
@@ -17,8 +20,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
+
+app.use(express.cookieParser());
+app.use(express.session({secret: 'No flag? Goes in the bag! Red flag? RUN.'}));
+
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 // development only
 if ('development' == app.get('env')) {
@@ -31,8 +38,15 @@ app.get('/view/:scriptID', routes.view); // View a script with a cute page
 app.get('/raw/:scriptID', routes.raw);   // Request the raw data
 app.get('/meta/:scriptID', routes.meta); // Request metadata only
 
-app.get('/list', routes.list);          // View a script list prettily
+app.get('/list', routes.list);           // View a script list prettily
 app.get('/list/raw', routes.rawlist);    // Raw format of the list
+
+app.get('/manage', manage.manage);       // Manage script->save assocs
+app.post('/manage/add/post', manage.post_addassoc); // Add a new assoc
+app.post('/manage/rm/post', manage.post_rmassoc); // Remove assoc
+app.get('/assocs/:id', manage.get_assocdata); // Get assoc element
+
+app.get('/login', tptapi.login); // Manage tptapi login
 
 
 http.createServer(app).listen(app.get('port'), function(){
